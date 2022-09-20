11b39c9666c2f860c7362983c57b26f26d9205b2
BUGRA <camlibelbugrahan@gmail.com>
2022-09-20 18:10:15 +0300

    Recovered files

---
+++ lib/home/home.dart
@@ -0,0 +1,105 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/home/movie_exception.dart';
+import 'package:flutter_movie_app/home/movie_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'movie.dart';
+
+final moviesFutureProvider =
+    FutureProvider.autoDispose<List<Movie>>((ref) async {
+  ref.maintainState = true;
+  final movieService = ref.read(movieServiceProvider);
+  final movies = await movieService.getMovies();
+  return movies;
+});
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key, required String title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: const Text('Moviiies'),
+      ),
+      body: ref.watch(moviesFutureProvider).when(
+            error: (e, s) {
+              if (e is MoviesException) {
+                // return _ErrorBody(message: e.message);
+              }
+
+              return const Text('error'); // yine error body yapılcak
+            },
+            loading: () => const Center(child: CircularProgressIndicator()),
+            data: (movies) {
+              return GridView.extent(
+                maxCrossAxisExtent: 200,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 0.7,
+                children:
+                    movies.map((movie) => _MovieBox(movie: movie)).toList(),
+              );
+            },
+          ),
+    );
+  }
+}
+
+class _MovieBox extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieBox({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.network(
+          movie.fullImageUrl,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: _FrontBanner(text: movie.title),
+        ),
+      ],
+    );
+  }
+}
+
+class _FrontBanner extends StatelessWidget {
+  const _FrontBanner({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          color: Colors.grey.shade200.withOpacity(0.5),
+          height: 60,
+          child: Center(
+            child: Text(
+              text,
+              style: Theme.of(context).textTheme.bodyText2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
